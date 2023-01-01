99f7185980066981581ae93bff579e065cfa1b8c
albertlast <mathiaspapealbert@hotmail.com>
2023-01-01 15:48:30 +0100

    PartialCounter, fullscreen, wakelock

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,89 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
 import PlayerView from './components/PlayerView.vue'
+const FullscreenEnabled = ref<boolean>(false)
+const ScreenOffEnabled = ref<boolean>(false)
+let wakeLock: null | AbortController = null
+
+const showSettings = ref<boolean>(false)
+const errorSceenOff = ref<string | null>(null)
+
+watch(FullscreenEnabled, async (newFullscreenEnabled) => {
+  if (newFullscreenEnabled) document.documentElement.requestFullscreen()
+  else document.exitFullscreen()
+})
+
+const requestWakeLock = async () => {
+  try {
+    wakeLock = await navigator.wakeLock.request('screen')
+    wakeLock.addEventListener('release', (e) => {
+      console.log(e)
+      console.log('Wake Lock was released')
+      // errorSceenOff.value = 'Wake Lock was released'
+
+      ScreenOffEnabled.value = false
+    })
+    // errorSceenOff.value = 'Wake Lock is active'
+    console.log('Wake Lock is active')
+  } catch (e) {
+    errorSceenOff.value = `${e.name}, ${e.message}`
+    console.error(`${e.name}, ${e.message}`)
+  }
+}
+
+watch(ScreenOffEnabled, (newScreenOffEnabled) => {
+  if (newScreenOffEnabled) {
+    wakeLock = requestWakeLock()
+  } else if (wakeLock) {
+    wakeLock.release()
+    wakeLock = null
+  }
+})
 </script>
 
 <template>
   <main class="w-screen h-screen">
-    <PlayerView class="rotate-180 bg-cyan-900" style="height: 50vh" />
-    <PlayerView class="bg-yellow-600" style="height: 50vh" />
+    <div
+      id="openSettings"
+      class="openSetting"
+      @click.stop="showSettings = !showSettings"
+    >
+      #
+    </div>
+    <div id="setting" class="setting bg-red-800" v-if="showSettings">
+      <div>Fullscreen</div>
+      <input type="checkbox" v-model="FullscreenEnabled" />
+      <div>Screen-Off</div>
+      <input type="checkbox" v-model="ScreenOffEnabled" />
+      <div v-if="errorSceenOff">{{ errorSceenOff }}</div>
+      <div @click.stop="showSettings = !showSettings">close</div>
+    </div>
+    <PlayerView
+      class="PlayerView rotate-180 bg-cyan-900"
+      style="height: 50vh"
+    />
+    <PlayerView class="PlayerView bg-yellow-600" style="height: 50vh" />
   </main>
 </template>
 
 <style>
-main {
-  font-size: min(30vh, 30vw);
+.PlayerView {
+  font-size: min(28vh, 30vw);
+}
+</style>
+<style scoped>
+.setting {
+  position: absolute;
+  inset: 0;
+  z-index: 10;
+  font-size: min(14vh, 14vw);
+}
+.openSetting {
+  position: absolute;
+  top: 50%;
+  right: 50%;
+  transform: translate(50%, -50%);
+  z-index: 5;
+  font-size: min(14vh, 14vw);
 }
 </style>
--- src/components/PlayerView.vue
+++ src/components/PlayerView.vue
@@ -4,18 +4,49 @@ import { ref } from 'vue'
 const startLive = ref<number>(20)
 const CurrentLive = ref(startLive.value)
 
+const partValueShow = ref<boolean>(false)
+let partValueTimeOutID: number | null = null
+const partValueVal = ref<number>(0)
+
+const updatePartValue = async (val: number) => {
+  if (partValueShow.value === false) {
+    partValueVal.value = 0
+    partValueShow.value = true
+  }
+  partValueVal.value += val
+  if (partValueTimeOutID) clearTimeout(partValueTimeOutID)
+  partValueTimeOutID = setTimeout(() => {
+    partValueShow.value = false
+  }, 2000)
+}
+
 const redurceLive = () => {
   CurrentLive.value--
+  updatePartValue(-1)
 }
 const addLive = () => {
   CurrentLive.value++
+  updatePartValue(1)
 }
 </script>
 
 <template>
   <div class="grid grid-cols-3 w-full">
     <div @click.stop="redurceLive" class="flex items-center"><p>-</p></div>
-    <div class="flex items-center">{{ CurrentLive }}</div>
+    <div class="grid">
+      <div class="small" :class="{ invisible: !partValueShow }">
+        {{ partValueVal }}
+      </div>
+      <div>{{ CurrentLive }}</div>
+    </div>
     <div @click.stop="addLive" class="flex items-center justify-end">+</div>
   </div>
 </template>
+<style scoped>
+.small {
+  font-size: min(5vh, 10vw);
+}
+.invisible {
+  opacity: 0;
+}
+</style>
