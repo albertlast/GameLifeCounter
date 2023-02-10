1bf041989c33b33697fb5fb38725e4b723f73f51
albertlast <mathiaspapealbert@hotmail.com>
2023-02-10 16:17:22 +0100

    pwa added

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: '/icons/heart.png',
+            sizes: '50x50',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   base: './',
   resolve: {
     alias: {
